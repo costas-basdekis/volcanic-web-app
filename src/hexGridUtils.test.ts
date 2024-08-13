4739ccd0ce7ab789fcdf5dffbf865556869b8c04
Costas Basdekis <costas.basdekis@gmail.com>
2024-08-13 20:27:33 +0100

    Calculate surrounding tiles for a position

--- src/hexGridUtils.test.ts
+++ src/hexGridUtils.test.ts
@@ -1,4 +1,11 @@
-import {getTilePosition} from "./hexGridUtils";
+import {
+  getBottomLeftPosition,
+  getBottomRightPosition,
+  getLeftPosition,
+  getRightPosition,
+  getSurroundingPositions,
+  getTilePosition, getTopLeftPosition, getTopRightPosition, Position
+} from "./hexGridUtils";
 
 describe("getTilePosition", () => {
   it("gets tile position for {0, 0}", () => {
@@ -16,4 +23,63 @@ describe("getTilePosition", () => {
   it("gets tile position for {0, 2}", () => {
     expect(getTilePosition({x: 0, y: 2}, 100)).toEqualCloseTo({x: 0, y: 300})
   });
+});
+
+describe("getSurroundingPositions", () => {
+  interface NamedPositions {
+    center: Position,
+    right: Position,
+    bottomRight: Position,
+    bottomLeft: Position,
+    left: Position,
+    topLeft: Position,
+    topRight: Position,
+  }
+  const evenPositions: NamedPositions = {
+    center: {x: 0, y: 0},
+    right: {x: 1, y: 0},
+    bottomRight: {x: 0, y: 1},
+    bottomLeft: {x: -1, y: 1},
+    left: {x: -1, y: 0},
+    topLeft: {x: -1, y: -1},
+    topRight: {x: 0, y: -1},
+  }
+  const oddPositions: NamedPositions = {
+    center: {x: 0, y: 1},
+    right: {x: 1, y: 1},
+    bottomRight: {x: 1, y: 2},
+    bottomLeft: {x: 0, y: 2},
+    left: {x: -1, y: 1},
+    topLeft: {x: 0, y: 0},
+    topRight: {x: 1, y: 0},
+  }
+  for (const item of [["even", evenPositions], ["odd", oddPositions]] as [string, NamedPositions][]) {
+    (([name, positions]) => {
+      describe(`on ${name} rows`, () => {
+        const {center, right, bottomRight, bottomLeft, left, topLeft, topRight} = positions;
+        it("gets surrounding positions", () => {
+          expect(getSurroundingPositions(center)).toEqual([
+            right,
+            bottomRight,
+            bottomLeft,
+            left,
+            topLeft,
+            topRight,
+          ]);
+        });
+        it("goes left <-> right", () => {
+          expect(getRightPosition(center)).toEqual(right);
+          expect(getLeftPosition(right)).toEqual(center);
+        });
+        it("goes top-left <-> bottom-right", () => {
+          expect(getBottomRightPosition(center)).toEqual(bottomRight);
+          expect(getTopLeftPosition(bottomRight)).toEqual(center);
+        });
+        it("goes top-right <-> bottom-left", () => {
+          expect(getBottomLeftPosition(center)).toEqual(bottomLeft);
+          expect(getTopRightPosition(bottomLeft)).toEqual(center);
+        });
+      });
+    })(item);
+  }
 });
--- src/hexGridUtils.ts
+++ src/hexGridUtils.ts
@@ -12,3 +12,42 @@ export const getTilePosition = (position: Position, size: number): Position => {
     y: offsetY * Math.cos(Math.PI / 3) * size * 3,
   };
 };
+
+export const getRightPosition = ({x, y}: Position): Position => {
+  return {x: x + 1, y};
+};
+
+export const getLeftPosition = ({x, y}: Position): Position => {
+  return {x: x - 1, y};
+};
+
+export const getTopRightPosition = ({x, y}: Position): Position => {
+  const evenRow = y % 2 === 0;
+  return {x: x + (evenRow ? 0 : 1), y: y - 1};
+};
+
+export const getTopLeftPosition = ({x, y}: Position): Position => {
+  const evenRow = y % 2 === 0;
+  return {x: x - (evenRow ? 1 : 0), y: y - 1};
+};
+
+export const getBottomRightPosition = ({x, y}: Position): Position => {
+  const evenRow = y % 2 === 0;
+  return {x: x + (evenRow ? 0 : 1), y: y + 1};
+};
+
+export const getBottomLeftPosition = ({x, y}: Position): Position => {
+  const evenRow = y % 2 === 0;
+  return {x: x - (evenRow ? 1 : 0), y: y + 1};
+};
+
+export const getSurroundingPositions = (position: Position): Position[] => {
+  return [
+    getRightPosition(position),
+    getBottomRightPosition(position),
+    getBottomLeftPosition(position),
+    getLeftPosition(position),
+    getTopLeftPosition(position),
+    getTopRightPosition(position),
+  ];
+};

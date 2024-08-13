97de5e23003f78ce350b9ce647a22b7e2eed0449
Costas Basdekis <costas.basdekis@gmail.com>
2024-08-13 09:54:24 +0100

    Remove unnecessary images, make it class-based

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("works", () => {
+    expect(true).toBe(true);
+  });
 });

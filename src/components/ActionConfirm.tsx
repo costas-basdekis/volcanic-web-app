f80e59a908b591fb111abbd2cdf33833bdc461fa
Costas Basdekis <costas.basdekis@gmail.com>
2024-08-22 16:16:49 +0100

    Add shortcuts for confirm/undo move

--- src/components/ActionConfirm.tsx
+++ src/components/ActionConfirm.tsx
@@ -1,4 +1,5 @@
 import "./ActionConfirm.css";
+import {useAutoShortcut} from "../hooks";
 
 export type MoveStage = "move-start" | "move-middle" | "move-end";
 
@@ -10,10 +11,12 @@ export interface ActionConfirmProps {
 
 export function ActionConfirm(props: ActionConfirmProps) {
   const {moveStage, onUndo, onConfirm} = props;
+  useAutoShortcut(onUndo, ["u"], "Undo move", "Undo part of a move");
+  useAutoShortcut(onConfirm, ["c"], "Confirm move", "Confirm move and commit it");
   return (
     <div className={"action-confirm"}>
-      <button disabled={moveStage === "move-start"} onClick={onUndo}>Undo</button>
-      <button disabled={moveStage !== "move-end"} onClick={onConfirm}>Confirm</button>
+      <button disabled={moveStage === "move-start"} onClick={onUndo}>Undo [U]</button>
+      <button disabled={moveStage !== "move-end"} onClick={onConfirm}>Confirm [C]</button>
     </div>
   )
 }
